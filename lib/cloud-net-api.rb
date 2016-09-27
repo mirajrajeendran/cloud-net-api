f30a12b16bd959a7d74af5b545811391267e9953
miraj <miraj@nuventure.in>
2016-09-27 16:24:02 +0530

    using staging api for development

--- lib/cloud-net-api.rb
+++ lib/cloud-net-api.rb
@@ -3,12 +3,14 @@ require 'faraday'
 require 'json'
 module CloudNetApi
 
-  API_ENDPOINT = "https://api.cloud.net"
+  # API_ENDPOINT = "https://api.cloud.net" its used for live data
+  API_ENDPOINT = "https://api.staging.cloud.net/"
 
   class CloudNet
 
     def initialize auth_string
       @authentication_string = auth_string
+      @connection = Faraday.new(API_ENDPOINT,{ssl: {verify: false}}) #its only for staging account
     end
 
     def self.setup mail_id, api_secret
@@ -17,11 +19,15 @@ module CloudNetApi
     end
 
     def get_all_datacenters
-      resp = Faraday.get("#{API_ENDPOINT}/datacenters") do |req|
+      #initial request for get response headers
+      
+      resp = @connection.get("#{API_ENDPOINT}/datacenters") do |req|
         req.headers["Authorization"] = "Basic #{@authentication_string}" 
       end
       total_results = resp.headers["x-total"]
-      full_data = Faraday.get("#{API_ENDPOINT}/datacenters") do |req|
+      #get all in one request
+      
+      full_data = @connection.get("#{API_ENDPOINT}/datacenters") do |req|
         req.params["per_page"] = total_results.to_i
         req.params["pege"] = 1
         req.headers["Authorization"] = "Basic #{@authentication_string}" 
@@ -30,7 +36,7 @@ module CloudNetApi
     end
 
     def get_datacenter id
-      resp = Faraday.get("#{API_ENDPOINT}/datacenters/#{id}") do |req|
+      resp = @connection.get("#{API_ENDPOINT}/datacenters/#{id}") do |req|
         req.headers["Authorization"] = "Basic #{@authentication_string}" 
       end
       return JSON.parse(resp.body)
